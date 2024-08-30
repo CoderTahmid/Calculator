37d8bc1bc9ddc7d260b5586b73692d25e68d9157
CoderTahmid <tahmid.ibne@gmail.com>
2024-08-30 19:40:05 +0600

    a little change done in JS file

--- app.js
+++ app.js
@@ -1,153 +1,135 @@
 let calcDisplay = document.querySelector(".calc-display");
 let calcButton = document.querySelector(".calc-button");
-let buttonPercent = document.querySelector(".button-percent");
-let buttonC = document.querySelector(".button-c");
 let clearButton = document.querySelector(".clear-button");
-let buttonXSquare = document.querySelector(".button-x-square");
-let button7 = document.querySelector(".button-7");
-let button8 = document.querySelector(".button-8");
-let button9 = document.querySelector(".button-9");
-let devideButton = document.querySelector(".button-devide");
-let button4 = document.querySelector(".button-4");
-let button5 = document.querySelector(".button-5");
-let button6 = document.querySelector(".button-6");
-let intoButton = document.querySelector(".button-into");
-let button1 = document.querySelector(".button-1");
-let button2 = document.querySelector(".button-2");
-let button3 = document.querySelector(".button-3");
-let minusButton = document.querySelector(".button-minus");
-let dotButton = document.querySelector(".button-dot");  
-let button0 = document.querySelector(".button-0");
-let plusButton = document.querySelector(".button-plus");
+let x = true;
 
-buttonPercent.addEventListener("click", () => {
+document.querySelector(".button-percent").addEventListener("click", () => {
     if (calcDisplay.value === ""){
         calcDisplay.value = "%";
     } else {
-        calcDisplay.value = calcDisplay.value + "%";
+        calcDisplay.value+="%";
     };
+
+    // if (x = true) {
+    //     clearButton.addEventListener("click", () => {
+    //         console.log("king");
+    //     })
+    // }
 });
 
-buttonC.addEventListener("click", () => {
+document.querySelector(".button-c").addEventListener("click", () => {
     calcDisplay.value = "";
 });
-clearButton.addEventListener("click", () => {
-    console.log("king");
-});
-buttonXSquare.addEventListener("click", () => {
+document.querySelector(".button-x-square").addEventListener("click", () => {
     if (calcDisplay.value === ""){
         calcDisplay.value = "";
     } else {
-        calcDisplay.value = calcDisplay.value * calcDisplay.value;
+        calcDisplay.value*=calcDisplay.value;
     };
 });
-button7.addEventListener("click", () => {
+document.querySelector(".button-7").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "7";
+        calcDisplay.value = 7;
     } else {
-        calcDisplay.value = calcDisplay.value + "7";
+        calcDisplay.value+=7; // calcDisplay.value = calcDisplay.value + 7;
     };
 });
-button8.addEventListener("click", () => {
+document.querySelector(".button-8").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "8";
+        calcDisplay.value = 8;
     } else {
-        calcDisplay.value = calcDisplay.value + "8";
+        calcDisplay.value+=8;
     };
 });
-button9.addEventListener("click", () => {
+document.querySelector(".button-9").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "9";
+        calcDisplay.value = 9;
     } else {
-        calcDisplay.value = calcDisplay.value + "9";
+        calcDisplay.value+=9;
     };
 });
-devideButton.addEventListener("click", () => {
+document.querySelector(".button-devide").addEventListener("click", () => {
     if (calcDisplay.value === ""){
         calcDisplay.value = "/";
     } else {
-        calcDisplay.value = calcDisplay.value + "/";
+        calcDisplay.value+="/";
     };
 });
-button4.addEventListener("click", () => {
+document.querySelector(".button-4").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "4";
+        calcDisplay.value = 4;
     } else {
-        calcDisplay.value = calcDisplay.value + "4";
+        calcDisplay.value+=4;
     };
 });
-button5.addEventListener("click", () => {
+document.querySelector(".button-5").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "5";
+        calcDisplay.value = 5;
     } else {
-        calcDisplay.value = calcDisplay.value + "5";
+        calcDisplay.value+=5; 
     };
 });
-button6.addEventListener("click", () => {
+document.querySelector(".button-6").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "6";
+        calcDisplay.value = 6;
     } else {
-        calcDisplay.value = calcDisplay.value + "6";
+        calcDisplay.value+=6;
     };
 });
-intoButton.addEventListener("click", () => {
+document.querySelector(".button-into").addEventListener("click", () => {
     if (calcDisplay.value === ""){
         calcDisplay.value = "*";
     } else {
-        calcDisplay.value = calcDisplay.value + "*";
+        calcDisplay.value+="*";
     };
 });
-button1.addEventListener("click", () => {
+document.querySelector(".button-1").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "1";
+        calcDisplay.value = 1;
     } else {
-        calcDisplay.value = calcDisplay.value + "1";
+        calcDisplay.value+=1;
     };
 });
-button2.addEventListener("click", () => {
+document.querySelector(".button-2").addEventListener("click", () => {
     if (calcDisplay.value === ""){
         calcDisplay.value = "2";
     } else {
-        calcDisplay.value = calcDisplay.value + "2";
+        calcDisplay.value+=2;
     };
 });
-button3.addEventListener("click", () => {
+document.querySelector(".button-3").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "3";
+        calcDisplay.value = 3;
     } else {
-        calcDisplay.value = calcDisplay.value + "3";
+        calcDisplay.value+=3;
     };
 });
-minusButton.addEventListener("click", () => {
+document.querySelector(".button-minus").addEventListener("click", () => {
     if (calcDisplay.value === ""){
         calcDisplay.value = "-";
     } else {
-        calcDisplay.value = calcDisplay.value + "-";
+        calcDisplay.value+="-";
     };
 });
-dotButton.addEventListener("click", () => {
+document.querySelector(".button-dot").addEventListener("click", () => {
     if (calcDisplay.value === ""){
         calcDisplay.value = ".";
     } else {
-        calcDisplay.value = calcDisplay.value + ".";
+        calcDisplay.value+=".";
     };
 });
-button0.addEventListener("click", () => {
+document.querySelector(".button-0").addEventListener("click", () => {
     if (calcDisplay.value === ""){
-        calcDisplay.value = "0";
+        calcDisplay.value = 0;
     } else {
-        calcDisplay.value = calcDisplay.value + "0";
+        calcDisplay.value+=0;
     };
 });
-plusButton.addEventListener("click", () => {
+document.querySelector(".button-plus").addEventListener("click", () => {
     if (calcDisplay.value === "") {
         calcDisplay.value = "+";
     } else {
-        calcDisplay.value = calcDisplay.value + "+";
+        calcDisplay.value+="+";
     };
 });
-
-
-
-
 
